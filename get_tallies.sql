cb93ee1ad706195286260a29bd90934c0fd6bb60
Sean C. Rife <seanrife@gmail.com>
2020-02-12 20:40:10 -0600

    Delete get_tallies.sql

--- get_tallies.sql
+++
@@ -1,4 +0,0 @@
-SELECT wiki_data.page_id, citation_tallies.doi, supporting, contradicting, mentioning, unclassified
-FROM citation_tallies
-INNER JOIN wiki_data
-ON citation_tallies.doi = wiki_data.doi;
