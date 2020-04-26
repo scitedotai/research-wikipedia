74a9e1ed9451d6c0cfbe59cb20e8c69ea6f57d31
Sean C. Rife <seanrife@gmail.com>
2020-04-26 11:07:25 -0500

    Update to get distinct doi counts

--- Code/get_retracted.sql
+++ Code/get_retracted.sql
@@ -1,1 +1,10 @@
-select wiki_data.page_title, wiki_data.doi, doidata.retracted from doidata inner join wiki_data on doidata.doi = wiki_data.doi where doidata.retracted = true;
+-- get list of wikipedia articles that have been retracted
+select wiki_data.page_title, wiki_data.doi, doidata.retracted from doidata
+inner join wiki_data on doidata.doi = wiki_data.doi
+where doidata.retracted = true;
+
+-- get count of unique, retracted dois referenced by wikipedia
+select distinct wiki_data.doi
+from doidata
+inner join wiki_data on doidata.doi = wiki_data.doi
+where doidata.retracted = true;
